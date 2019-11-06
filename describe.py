4139538b8f2e944b9501634bc5e6f52d8fd914f4
t0ny <anamsell@student.42.fr>
2019-11-06 11:58:20 +0100

    git ignore

--- describe.py
+++ describe.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     file_name = MLKit.CommandLine.get_file_name()
     data_table = MLKit.DataTable(file_name)
     data_table.compute_columns_atributes()
-    data_table.display_attributes(0, 6)
+    data_table.display_attributes(from_index=6)
 
     print(data_table.values_for_target_column_named("Hogwarts House", ["Ravenclaw"], "Best Hand"))
--- histogram.py
+++ histogram.py
@@ -2,10 +2,14 @@ import matplotlib
 import MLKit
 import csv
 
+
 if __name__ == "__main__":
     file_name = MLKit.CommandLine.get_file_name()
     try :
         f = open(file_name)
     except FileNotFoundError:
         MLKit.Display.error("Incorrect name of file.")
-    reader = list()
+    reader = list(csv.DictReader(f))
+    for line in reader:
+        hogwarts_house = line.get('Hogwarts House')
+
