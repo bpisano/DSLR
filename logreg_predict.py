80763d42d0ee25f85ca868c2464c4fc91a012dd4
Benjamin Pisano <benjamin.pisano@icloud.com>
2020-09-15 17:39:42 +0200

    Fixed save

--- logreg_predict.py
+++ logreg_predict.py
@@ -3,10 +3,11 @@ import MLKit
 if __name__ == "__main__":
     default_target_column = "Hogwarts House"
     default_model = "train.mlmodel"
+    default_csv = "predict.csv"
 
     MLKit.CommandLine.register_flag("Y", description="The target column where the values should be predicted.", default_value=default_target_column)
     MLKit.CommandLine.register_flag("m", description="The model file name used to predict values.", default_value=default_model)
-    MLKit.CommandLine.register_flag("s", description="The csv file name with the predicted values.")
+    MLKit.CommandLine.register_flag("s", description="The csv file name with the predicted values.", default_value=default_csv)
     MLKit.CommandLine.register_usage("logreg_predict.py [csv_file_name]\nPredict the value of a csv data file from a trained model.")
     MLKit.CommandLine.show_usage_if_needed()
 
@@ -18,6 +19,4 @@ if __name__ == "__main__":
     data_table = MLKit.DataTable(file_name)
     data_table.compute_columns_attributes()
     data_table.predict(target_column_name, model_file_name)
-
-    if not output_csv is None:
-        data_table.save(output_csv)
+    data_table.save(output_csv)
